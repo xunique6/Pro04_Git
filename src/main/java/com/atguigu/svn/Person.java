9529a052ebc771018e4993d2191bf0b326af7a58
xunique6 <xunique6@sina.com>
2018-12-20 14:18:54 +0800

    添加一些文件

---
+++ src/main/java/com/atguigu/svn/Person.java
@@ -0,0 +1,13 @@
+package com.atguigu.svn;
+
+public class Person {
+
+	public static void main(String[] args) {
+		System.out.println("aaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaa");//@@@@@@@
+		System.out.println("aaaaaaaaaaa");//xxxxxxxxxx
+		System.out.println("aaaaaaaaaaa");//*********
+		System.out.println("aaaaaaaaaaa");// edit by  right
+	}
+}
